cae0eb3dc11ce981315a7cf4c1d5be449aaa12d2
Ossiggy <mrossig201@gmail.com>
2018-04-03 21:28:46 -0400

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, withRouter} from 'react-router-dom';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+     <div className="app">
+
+     </div>
+    );
+  }
+}
+
+export default withRouter(connect()(App));
